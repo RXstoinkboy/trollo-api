3036cfae6a61c2f6247e305b205c95e4adf96e66
RXstoinkboy <erykludwin@gmail.com>
2020-05-15 23:00:06 +0200

    changed users tables

--- sql/users.sql
+++ sql/users.sql
@@ -1,18 +1,22 @@
 -- Table: public.users
 
--- DROP TABLE public.users;
+-- username, password and information if user is active or not to indicate if it has been deleted
 
--- users list
+-- DROP TABLE public.users;
 
 CREATE TABLE public.users
 (
     id integer NOT NULL DEFAULT nextval('users_id_seq'::regclass),
     public_id text COLLATE pg_catalog."default" NOT NULL,
     login text COLLATE pg_catalog."default",
-    active boolean,
-    created_at timestamp without time zone,
-    updated_at timestamp without time zone,
+    active boolean DEFAULT true,
+    password_hash text COLLATE pg_catalog."default" NOT NULL,
+    password_salt text COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT users_login_key UNIQUE (login)
+,
+    CONSTRAINT users_password_hash_key UNIQUE (password_hash)
+,
+    CONSTRAINT users_password_salt_key UNIQUE (password_salt)
 ,
     CONSTRAINT users_public_id_key UNIQUE (public_id)
 
