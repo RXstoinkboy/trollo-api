99daf9171d82b20f04f94526e157e8be2ebb884b
RXstoinkboy <erykludwin@gmail.com>
2020-06-30 19:41:43 +0200

    pushing database structure

--- sql/categories.sql
+++ sql/categories.sql
@@ -8,6 +8,7 @@ CREATE TABLE public.categories
     public_id text COLLATE pg_catalog."default" NOT NULL,
     name text COLLATE pg_catalog."default" NOT NULL,
     created_at timestamp without time zone DEFAULT now(),
+    ui_position integer,
     CONSTRAINT categories_public_id_key UNIQUE (public_id)
 
 )
